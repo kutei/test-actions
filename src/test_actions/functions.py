4131f3d9d785f6559ba5a295354e796607e525fd
--get <50226240+kutei@users.noreply.github.com>
2021-10-14 17:06:43 +0900

    [fix]PEP8規約違反を修正

--- src/test_actions/functions.py
+++ src/test_actions/functions.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
+"""
+module doc
+"""
 
-import torch
-import os
-import numpy as np
 import platform
+import numpy as np
+
 
 def print_os_info():
     """
@@ -12,7 +14,9 @@ def print_os_info():
     print(platform.system())
     print(platform.release())
 
-def no_docstring(vara=None, varb = None):
+
+def no_docstring(vara=None, varb=None):
+    """ test docstring """
     print(vara)
     print(varb)
 
@@ -21,60 +25,23 @@ def warning_func():
     """
     違反コードを多く含む関数
     """
-    var0 = 0
-    var1 = var0
-    var2 = var1
-    var3 = var2
-    var4 = var3
-    var5 = var4
-    var6 = var5
-    var7 = var6
-    var8 = var7
-    var9 = var8
-    vara = var8
-    varb = vara
-    varc = varb
-    vard = varc
-    vare = vard
-    varf = vare
-    varg = varf
-    vare = varg
+    var_0 = 0
 
     # hyper nested
-    for n in range(10):
+    for i in range(10):
         for _ in range(5):
             for _ in range(3):
-                for _ in range(2):
-                    for _ in range(2):
-                        for _ in range(2):
-                            var9 += n
+                var_0 += i
 
-    no_docstring(vara="5", varb = "6")
+    no_docstring(vara="5", varb="6")
     no_docstring(vara="5", varb="6")
 
     var_a = np.ndarray((5, 8))
     this_variable_is_named_to_create_long_line_above_80_characters = var_a.argmax()
-    print(this_variable_is_named_to_create_long_line_above_80_characters, "######################102")
-    print(this_variable_is_named_to_create_long_line_above_80_characters, "######################102", "#############122")
+    print(this_variable_is_named_to_create_long_line_above_80_characters)
 
     # too many returns and branches
-    if var0 is None:
+    if var_0 is None:
         return 0
-    elif var1 is None:
-        return 1
-    elif var2 is None:
-        return 2
-    elif var3 is None:
-        return 3
-    elif var4 is None:
-        return 4
-    elif var5 is None:
-        return 5
-    elif var6 is None:
-        return 6
-    elif var7 is None:
-        return 7
-    elif var8 is None:
-        return 8
-    elif var9 is None:
-        return 9
+
+    return 9
